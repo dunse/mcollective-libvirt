8f20a03dbfbcea61e39b087338733203a2323b88
Darren Worrall <darren@iweb.co.uk>
2011-01-05 17:42:03 +0000

    Added status action

--- mclibvirt.ddl
+++ mclibvirt.ddl
@@ -7,6 +7,7 @@ metadata :name        => "SimpleRPC libvirt agent",
          :timeout     => 10
  
 action "list", :description => "List running domains" do
+    display :always
     input :all,
           :prompt      => "List all?",
           :description => "List all domains, not just running ones",
@@ -16,4 +17,19 @@ action "list", :description => "List running domains" do
     output :data,
            :description => "Matching domains",
            :display_as  => "Domains"
+end
+
+action "status", :description => "Report the status of a given domain" do
+    display :ok
+    input :name,
+          :prompt      => "Domain name",
+          :description => "The name of the domain you're interested in.",
+          :type        => :string,
+          :validation  => '^[a-zA-Z\-_\d]+$',
+          :maxlength   => 30,
+          :optional    => false
+ 
+    output :data,
+           :description => "Status of the domain",
+           :display_as  => "Status:"
 end
--- mclibvirt.rb
+++ mclibvirt.rb
@@ -10,10 +10,10 @@ module MCollective
                      :version     => "0.1",
                      :url         => "http://will.push.to.github.if.it.works",
                      :timeout     => 10
+ 
 
             # Provide some basic libvirt functionality
             action "list" do
-#                validate :all, Boolean
                 conn = Libvirt::open("qemu:///system")
                 doms = conn.list_domains
                 result = []
@@ -25,6 +25,26 @@ module MCollective
                     result.concat(conn.list_defined_domains)
                 end 
                 reply.data = result.join(" | ")
+            end
+
+            action "status" do
+                validate :name, String
+                conn = Libvirt::open("qemu:///system")
+                begin
+                    virdomainstate = { 0 => "No state",
+                                     1 => "Running",
+                                     2 => "Blocked on resource",
+                                     3 => "Paused",
+                                     4 => "Shutting down",
+                                     5 => "Shut off",
+                                     6 => "Crashed"
+                    }
+                    domain = conn.lookup_domain_by_name(request[:name])
+                    reply.data = virdomainstate[domain.info.state]
+                rescue Libvirt::RetrieveError => e
+                    reply.fail "Unable to lookup domain #{request[:name]}", 1
+                    return
+                end
             end
 
         end
